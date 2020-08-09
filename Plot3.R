900bbb8ae68c8e48062b71c50fb0d5aeaacb31db
Dinara <63246554+Dinasdfg@users.noreply.github.com>
2020-08-09 21:17:44 +0300

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -5,5 +5,5 @@ SCC <- readRDS("Source_Classification_Code.rds")
 library(dplyr)
 by_type <- NEI %>% subset(NEI$fips == "24510") %>% group_by(year, type) %>% summarise(Emissions=sum(Emissions))
 library(ggplot2)
-plot <- ggplot(by_type, aes(x=year, y=Emissions, fill=year)) + geom_bar(stat = "identity")+ xlab("Year") + ylab("Total Emissions") + ggtitle("Total Annual Emissions in Baltimore by Year")
+plot <- ggplot(by_type, aes(x=year, y=Emissions, fill=type)) + geom_bar(stat = "identity")+ xlab("Year") + ylab("Total Emissions") + ggtitle("Total Annual Emissions in Baltimore by Year")+facet_grid(. ~ type)+scale_fill_manual(values = c("#8C96B9", "#B3CDE3", "#8C96C1", "#88419D"))+theme_minimal()
 plot
